fbbac6b2052f0c11771c94db47f584a68e7b99b3
VG\tsivaramakrishnan <sivaramakrishnan.thiruvengadam-ext@solvay.com>
2024-03-01 12:28:15 +0530

    COMMIT 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   data!: any[];
   editedRowIndex!: number;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
     this.loadData();
